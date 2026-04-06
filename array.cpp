47669260de0e5726e9055811049e9ac5c48eda7a
zzaain <sheriellaghzain@gmail.com>
2026-04-06 10:37:37 +0700

    menambahkan akses dan tampilkan array

--- array.cpp
+++ array.cpp
@@ -9,5 +9,9 @@ int main() {
     int dataku[5] = {10, 20, 30, 40, 50};
     // deklarasi tanpa inisialisasi
     int datamu[5];
+    // cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : "
+     << dataku[1] << endl;   
+
     return 0;
 }
