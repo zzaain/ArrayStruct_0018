603bf82780ae95c7bf8296f71a84029dd5261332
zzaain <sheriellaghzain@gmail.com>
2026-04-06 11:33:14 +0700

    menambahkan struct alamat dan mahasiswa

---
+++ struct2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct alamatDetail
+{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
+int main()
+{
+    
+}
