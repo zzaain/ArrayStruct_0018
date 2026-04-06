60b2888de870b0667b80432a98dea8711b788ffd
zzaain <sheriellaghzain@gmail.com>
2026-04-06 10:48:21 +0700

    menambahkan input semua data array

--- array.cpp
+++ array.cpp
@@ -22,6 +22,13 @@ int main() {
     cin >> datamu[0];
     cout << "Isikan data kedua : ";
     cin >> datamu[1];
+    cout << "Isikan data ketiga : ";
+    cin >> datamu[2];
+    cout << "Isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "Isikan data kelima : ";
+    cin >> datamu[4];
+
 
     return 0;
 }
