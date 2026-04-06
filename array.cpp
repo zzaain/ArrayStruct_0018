20095bbc68c4c67ddafb36ce624d667593ea817c
zzaain <sheriellaghzain@gmail.com>
2026-04-06 10:49:42 +0700

    menambahkan output data satu per satu

--- array.cpp
+++ array.cpp
@@ -29,6 +29,11 @@ int main() {
     cout << "Isikan data kelima : ";
     cin >> datamu[4];
 
+  cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
 
     return 0;
 }
