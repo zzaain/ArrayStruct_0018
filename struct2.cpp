183b0f72ca51cb223101f45c02c41a34f940716d
zzaain <sheriellaghzain@gmail.com>
2026-04-06 11:34:06 +0700

    menambahkan deklarasi dan input data

--- struct2.cpp
+++ struct2.cpp
@@ -16,5 +16,16 @@ struct mahasiswa
 
 int main()
 {
-    
+    // deklarasi variable struct
+    mahasiswa mhs;
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Isikan data desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+
 }
