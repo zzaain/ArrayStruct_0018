7c97f25694dda380ae942f65adfba1fd45f85e13
zzaain <sheriellaghzain@gmail.com>
2026-04-06 11:39:53 +0700

    menambahkan struct dan input data

---
+++ struct3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct AlamatDetail
+{
+    string desa;
+    string kota;
+    
+};
+
+struct mahasiswa
+{
+    string nama;
+    AlamatDetail alamat;
+    int umur;
+};
+
+int main(){
+        // deklarasi variable struct
+    mahasiswa mhs;
+
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+
+}
